b0de434725edaa3d961e09195ef1e7792e2397dd
Thalia29 <jtc.guznay@yavirac.edu.ec>
2023-08-15 20:20:32 -0500

    cambios_david

--- src/router/inicio.router.js
+++ src/router/inicio.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Mostrar, mandar } = require('../controller/inicio.controller');
 const router = express.Router();
 
-router.get ('inicio', Mostrar)
-router.post ('/inicio', mandar)
+router.get ('/', Mostrar)
+router.post ('/', mandar)
 
 module.exports = router
