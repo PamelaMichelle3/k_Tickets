ebe83496c06e65f2bca7442de3c35d1e6f076093
PamelaMichelle3 <maiguamichelle0304@gmail.com>
2023-08-16 20:07:21 -0500

    cambiospame

--- src/router/buses.router.js
+++ src/router/buses.router.js
@@ -3,12 +3,6 @@ const { Mostrar, mandar, lista, traer, actualizar, eliminar } = require('../cont
 const rutas = express.Router();
 
 
-rutas.get('/buses', Mostrar)
-rutas.post("/buses/agregar/:id", mandar)
-rutas.get("/buses/lista", lista)
-rutas.get('/buses/eliminar/:id', eliminar)
-rutas.post('/buses/editar/:id', actualizar)
-rutas.get('/buses/editar/:id', traer)
-rutas.get('/buses/eliminar/:id', eliminar)
+
 
 module.exports = router
